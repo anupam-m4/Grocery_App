0a6cae8fed97ec4a675e1002e3e75c6de463d28f
Anupam Padhy <anupampadhy98@gmail.com>
2026-06-18 16:05:08 +0530

    Add Splash screen

---
+++ src/screens/Splash.tsx
@@ -0,0 +1,15 @@
+function Splash() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-emerald-500">
+      <div className="flex items-center gap-2 text-white">
+        <span className="text-3xl">🥕</span>
+        <div>
+          <h1 className="text-3xl font-bold leading-none">nectar</h1>
+          <p className="text-xs tracking-widest">online groceries</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Splash
